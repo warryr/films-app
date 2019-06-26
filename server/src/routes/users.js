82a5258a13eee71e74aad6dccdf3e3f3d20d04a6
Maria Rachkovskaya <43150866+warryr@users.noreply.github.com>
2019-06-26 11:24:02 +0300

    refactor: refactor error handling

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,14 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import { validate } from 'express-jsonschema';
-import checkIfUnique from '../middleware/checkIfUnique';
 
 import userSchema from '../schemas/user';
 import User from '../models/user';
 
 const router = express.Router();
 
-router.post('/register', validate({ body: userSchema }), checkIfUnique, async (req, res, next) => {
+router.post('/register', validate({ body: userSchema }), async (req, res, next) => {
   const user = new User({ _id: mongoose.Types.ObjectId(), ...req.body });
   await user.encryptPassword();
 
@@ -16,10 +15,23 @@ router.post('/register', validate({ body: userSchema }), checkIfUnique, async (r
     .save()
     .then(user => res.send({ username: user.username, email: user.email }))
     .catch(err => {
-      if (err.name === 'ValidationError') {
-        res.status(422).send(err.errors);
-      } else {
-        res.status(500).json(err);
+      switch (err.name) {
+        case 'ValidationError': {
+          const errors = {};
+          for (let field in err.errors) {
+            errors[field] = err.errors[field].message;
+          }
+          res.status(422).send(errors);
+          break;
+        }
+        case 'MongoError': {
+          const message = err.errmsg;
+          res.status(409).send(message);
+          break;
+        }
+        default: {
+          res.status(500).json(err);
+        }
       }
     });
 });
@@ -28,7 +40,7 @@ router.use((err, req, res, next) => {
   if (err.name === 'JsonSchemaValidation') {
     const errors = {};
     err.validations.body.map(object => (errors[object.property] = object.messages[0]));
-    res.status(400).send(err.validations);
+    res.status(400).send(errors);
   } else {
     next(err);
   }
