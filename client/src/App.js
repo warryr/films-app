22d048ff599b0f42c45a8d415b446a5297433ec4
Maria Rachkovskaya <43150866+warryr@users.noreply.github.com>
2019-07-01 15:44:24 +0300

    feat: add switch for routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
 import { history } from './reducers/store';
 import RegisterContainer from './containers/RegisterContainer';
 import LoginContainer from './containers/LoginContainer';
-import FilmsContainer from './containers/FilmsContainer';
+import CatalogContainer from './containers/CatalogContainer';
 
 function App() {
   return (
     <ConnectedRouter history={history} basename={process.env.PUBLIC_URL}>
-      <Route path='/register' component={RegisterContainer} />
-      <Route path='/login' component={LoginContainer} />
-      <Route path='/films' component={FilmsContainer} />
+      <Switch>
+        <Route path='/register' component={RegisterContainer} />
+        <Route path='/login' component={LoginContainer} />
+        <Route path='/films' component={CatalogContainer} />
+        <Redirect to='/login' />
+      </Switch>
     </ConnectedRouter>
   );
 }
