2260366440fe820ec43638f1b56c9bcae5c1c8c4
Maria Rachkovskaya <43150866+warryr@users.noreply.github.com>
2019-07-01 15:46:09 +0300

    feat: add catalog saga to index saga

--- client/src/sagas/indexSaga.js
+++ client/src/sagas/indexSaga.js
@@ -1,7 +1,8 @@
 import { all } from 'redux-saga/effects';
 import RegisterSaga from './registerSaga';
 import LoginSaga from './loginSaga';
+import CatalogSaga from './catalogSaga';
 
 export default function* indexSaga() {
-  yield all([RegisterSaga(), LoginSaga()]);
+  yield all([RegisterSaga(), LoginSaga(), CatalogSaga()]);
 }
