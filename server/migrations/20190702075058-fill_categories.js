5b680e8efd689d191a9ddcb35af14cd7d57f40df
Maria Rachkovskaya <43150866+warryr@users.noreply.github.com>
2019-07-02 15:37:45 +0300

    refactor: rename categories

--- server/migrations/20190702075058-fill_categories.js
+++ server/migrations/20190702075058-fill_categories.js
@@ -5,19 +5,19 @@ module.exports = {
     await db.collection('film-categories').insertMany([
       {
         _id: ObjectID('5d089d57a07666ef9cfed35f'),
-        title: "90's films",
+        title: "1990's films",
         description: 'Films that have been released in 1990-2000.',
         films: ['5d089b1d655a7c1ae81bb86a'],
       },
       {
         _id: ObjectID('5d089d57a07666ef9cfed35e'),
-        title: "00's films",
+        title: "2000's films",
         description: 'Films that have been released in 2000-2010.',
         films: ['5d089b1d655a7c1ae81bb86e'],
       },
       {
         _id: ObjectID('5d089d57a07666ef9cfed360'),
-        title: "10's films",
+        title: "2010's films",
         description: 'Films that have been released in 2010-2019.',
         films: ['5d089b1d655a7c1ae81bb86d', '5d089b1d655a7c1ae81bb86c', '5d089b1d655a7c1ae81bb86b'],
       },
