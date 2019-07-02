c2a7492881bfd084216e382ee0c9f6d68205bc00
Maria Rachkovskaya <43150866+warryr@users.noreply.github.com>
2019-07-02 16:00:52 +0300

    feat: add search by category

--- server/src/routes/films.js
+++ server/src/routes/films.js
@@ -3,13 +3,37 @@ import mongoose from 'mongoose';
 import passport from 'passport';
 
 import Film from '../models/film';
-import FilmCategory from '../models/filmCategory';
 
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
   try {
+    console.log(req.query);
+    console.log(req.query.category);
+
+    const searchConditions = req.query.category ? { category: req.query.category } : {};
+    const sortConditions = { _id: 1 };
+
+    console.log(searchConditions);
+
     // get all films or by category
+    const docs = await Film.find(
+      searchConditions,
+      {
+        title: 1,
+        description: 1,
+        avatar: 1,
+        category: 1,
+      },
+      {
+        $limit: 10,
+        $sort: sortConditions,
+      }
+    ).populate({ path: 'category', select: 'title _id' });
+
+    console.log(docs);
+
+    res.send(docs);
   } catch (err) {
     next(err);
   }
