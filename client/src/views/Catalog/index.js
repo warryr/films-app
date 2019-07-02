2f3d1901436f63feb8aa84e13c231537978cf501
Maria Rachkovskaya <43150866+warryr@users.noreply.github.com>
2019-07-02 15:35:00 +0300

    feat: add categories to catalog view

--- client/src/views/Catalog/index.js
+++ client/src/views/Catalog/index.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { useStyles } from './styles';
 import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
@@ -14,26 +16,49 @@ const Catalog = props => {
   return (
     <Container className={classes.paper}>
       <CssBaseline />
-      {props.loading ? (
+      {props.iLoading || props.cLoading ? (
         <Typography className={classes.title}>Loading...</Typography>
       ) : (
-        props.items.map((item, index) => (
-          <Card className={classes.card} key={index}>
-            <CardMedia className={classes.media} image={item.avatar} />
-            <CardContent className={classes.content}>
-              <Typography className={classes.title}>{item.title}</Typography>
-              <Typography className={classes.text}>{item.description}</Typography>
-              <Divider className={classes.divider} light />
-              <Typography className={classes.subtitle}>Category: {item.category}</Typography>
-            </CardContent>
-          </Card>
-        ))
+        <Container className={classes.container}>
+          <Container className={classes.menuContainer}>
+            <Box className={classes.menu}>
+              <Typography className={classes.title}>Categories</Typography>
+              <Button className={classes.category} onClick={() => props.getByCategory(0)}>
+                All
+              </Button>
+              {props.categories.map((category, index) => (
+                <Button key={index} className={classes.category} onClick={() => props.getByCategory(category._id)}>
+                  {category.title}
+                </Button>
+              ))}
+            </Box>
+          </Container>
+          <Container className={classes.cardsContainer}>
+            {props.items.map((item, index) => (
+              <Card className={classes.card} key={index}>
+                <CardMedia className={classes.media} image={item.avatar} />
+                <CardContent className={classes.content}>
+                  <Typography className={classes.title}>{item.title}</Typography>
+                  <Typography className={classes.text}>{item.description}</Typography>
+                  <Divider className={classes.divider} light />
+                  <Typography className={classes.subtitle}>{item.category.title}</Typography>
+                </CardContent>
+              </Card>
+            ))}
+          </Container>
+        </Container>
       )}
     </Container>
   );
 };
 
 Catalog.propTypes = {
+  categories: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+    })
+  ),
   items: PropTypes.arrayOf(
     PropTypes.shape({
       avatar: PropTypes.node.isRequired,
@@ -41,8 +66,10 @@ Catalog.propTypes = {
       description: PropTypes.string.isRequired,
       category: PropTypes.string.isRequired,
     })
-  ).isRequired,
-  loading: PropTypes.bool.isRequired,
+  ),
+  iLoading: PropTypes.bool.isRequired,
+  cLoading: PropTypes.bool.isRequired,
+  getByCategory: PropTypes.func.isRequired,
 };
 
 export default Catalog;
--- client/src/views/Catalog/styles.js
+++ client/src/views/Catalog/styles.js
@@ -8,17 +8,38 @@ export const useStyles = makeStyles(theme => ({
   },
   paper: {
     marginTop: theme.spacing(8),
+  },
+  container: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 3fr',
+    gridGap: theme.spacing(2),
+  },
+  menuContainer: {
+    width: '100%',
+    height: 'auto',
+    margin: theme.spacing(3),
+  },
+  cardsContainer: {
     display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    width: '100%',
+    height: 'auto',
+  },
+  menu: {
+    height: 'auto',
+    textAlign: 'center',
+    padding: theme.spacing(2),
+    boxShadow: '0 8px 30px 0 rgba(0,0,0,0.2)',
   },
   card: {
-    maxWidth: 300,
-    margin: 'auto',
+    maxWidth: '25%',
+    margin: theme.spacing(3),
     transition: '0.5s',
-    boxShadow: '0 8px 40px -12px rgba(0,0,0,0.3)',
+    boxShadow: '0 8px 30px 0 rgba(0,0,0,0.2)',
     '&:hover': {
-      boxShadow: '0 16px 70px -12.125px rgba(0,0,0,0.3)',
+      boxShadow: '0 16px 50px 5px rgba(0,0,0,0.2)',
     },
   },
   media: {
@@ -35,10 +56,20 @@ export const useStyles = makeStyles(theme => ({
   },
   subtitle: {
     fontSize: '16px',
-    color: theme.palette.primary.main,
+    color: theme.palette.secondary.dark,
   },
   text: {
     fontSize: '14px',
+  },
+  category: {
+    width: '100%',
+    fontSize: '16px',
+    fontWeight: 'bold',
+    color: theme.palette.primary.main,
+    textTransform: 'none',
+    '&:hover': {
+      backgroundColor: '#EEEEEE',
+    },
   },
   divider: {
     margin: `${theme.spacing(3)}px 0`,
